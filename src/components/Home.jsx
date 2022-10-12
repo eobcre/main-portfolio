e8a3823a68a3c83981c569a1d63bbaa31999222a
eobcre <eobcre@gmail.com>
2022-10-12 05:59:03 -0400

    Layout and background changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,45 +2,60 @@ import React from "react";
 import Brackets from "../img/brackets.png";
 import Bracket from "../img/bracket.png";
 import Bracket_2 from "../img/bracket_2.png";
+import Header from "./Header";
 
 const Home = () => {
   return (
     <div className="h-screen w-screen bg-webTop bg-center bg-zoom table-cell align-middle">
-      <main className="max-w-xs mx-auto">
-        <div className="grid">
-          <div className="subnames grid mt-12">
+      <Header />
+      <div className="text-center py-8">
+        <p className="text-7xl py-4">Emmy Manning</p>
+        <p className="text-xl">Love to build creative and magical things.</p>
+      </div>
+      <div className="glass-card mx-auto w-380 md:w-480">
+        <main className="max-w-xs mx-auto">
+          <div className="grid">
+            <div className="subnames grid mt-6">
+              <a
+                href="https://www.linkedin.com/in/emmymanning/"
+                target="_blank"
+                className="hover:underline text-light-gray"
+              >
+                <p className="text-light-gray text-sm uppercase">Me</p>
+              </a>
+              <a
+                href="#"
+                target="_blank"
+                className="hover:underline text-light-gray"
+              >
+                <p className="text-light-gray text-sm uppercase">Portfolio</p>
+              </a>
+            </div>
+            <img src={Brackets} alt="Icon" />
+            <p className="text-center text-3xl">Emmy@WebDevEM.com</p>
+          </div>
+          <img src={Bracket_2} alt="Icon" />
+          <div className="sub-ig grid">
             <a
-              href="https://www.linkedin.com/in/emmymanning/"
+              href="https://www.instagram.com/webdevcreative/"
+              target="_blank"
               className="hover:underline text-light-gray"
             >
-              <p className="text-light-gray text-xs uppercase">Me</p>
-            </a>
-            <a
-              href="https://www.instagram.com/codebecreative/"
-              className="hover:underline text-light-gray"
-            >
-              <p className="text-light-gray text-xs uppercase">Instagram</p>
+              <p className="text-light-gray text-sm uppercase">Instagram</p>
             </a>
           </div>
-          <img src={Brackets} alt="Icon" />
-          <p className="text-center text-2xl">Emmy@CodeBeCreative.com</p>
-        </div>
-        <img src={Bracket_2} alt="Icon" />
-        <div className="sub-web grid">
-          <a href="#" className="hover:underline text-light-gray">
-            <p className="text-light-gray text-xs uppercase">Website</p>
-          </a>
-        </div>
-        <img src={Bracket} alt="Icon" />
-        <div className="sub-email grid justify-center">
-          <a
-            href="mailto:eobcre@gmail.com"
-            className="hover:underline text-light-gray"
-          >
-            <p className="text-light-gray text-xs uppercase">Email</p>
-          </a>
-        </div>
-      </main>
+          <img src={Bracket} alt="Icon" />
+          <div className="sub-email grid justify-center mb-4">
+            <a
+              href="mailto:eobcre@gmail.com"
+              target="_blank"
+              className="hover:underline text-light-gray"
+            >
+              <p className="text-light-gray text-sm uppercase">Email</p>
+            </a>
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
