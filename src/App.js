794f41cc1d8cf860ab31dddfc387000e72ee9276
eobcre <eobcre@gmail.com>
2022-10-20 11:51:09 -0400

    Footer component added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <section id="Contact">
         <Contact name="Send Message" title="Contact" />
       </section>
+      <Footer />
     </div>
   );
 };
