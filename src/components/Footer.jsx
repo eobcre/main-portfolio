f5a0c5d246b0b6cd2917531f64b901b753ce1b8a
eobcre <eobcre@gmail.com>
2022-10-22 12:56:45 -0400

    Footer fixed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-light-blue h-60">
+      <div className="glass h-60"></div>
+    </div>
+  );
+};
+
+export default Footer;
