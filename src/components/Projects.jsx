f66eb38ca7c9f54771e2c9d8ddea49544c04e0bf
eobcre <eobcre@gmail.com>
2022-11-04 23:01:54 -0400

    Layout fixed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,10 +6,10 @@ import CaitzLogo from "../img/logo/caitz.png";
 const Projects = (prop) => {
   return (
     <div className="h-auto bg-light-blue py-20">
-      <div className="mx-auto w-4/5 md:w-3/4">
+      <div className="mx-auto w-4/5">
         <p className="sub-st">My Work</p>
-        <div className="grid gap-8">
-          <div className="glass w-auto">
+        <div className="gap-8 md:flex">
+          <div className="glass w-auto md:w-2/4">
             <span className="absolute md:-top-4 left-inset40 rotate-45 w-24 h-8 bg-dark-pink border-l-2 border-r-2 border-dotted"></span>
             <div className="px-4 pt-4">
               <img src={TodoLogo} alt="Logo" width="100" height="100" />
@@ -25,8 +25,8 @@ const Projects = (prop) => {
               </p>
             </div>
           </div>
-          <div className="glass w-auto mt-16">
-            <span className="absolute md:-top-4 left-inset40 rotate-45 w-24 h-8 bg-dark-pink md:border-l-2 md:border-r-2 md:border-dotted"></span>
+          <div className="glass mt-16 w-auto md:w-2/4 md:-mt-0">
+            <span className="absolute md:-top-4 left-inset40 rotate-45 w-24 h-8 bg-dark-pink border-l-2 border-r-2 border-dotted"></span>
             <div className="px-4 pt-4">
               <img src={CaitzLogo} alt="Logo" width="100" height="100" />
               <h1 className="text-3xl text-dark-gray pt-2">
@@ -34,10 +34,10 @@ const Projects = (prop) => {
                 <hr></hr>
               </h1>
               <p className="text-xl text-dark-gray my-4">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio,
-                reiciendis deserunt mollitia laborum doloribus nemo saepe rem
-                beatae accusamus praesentium cum temporibus dolorum ipsa nulla
-                quaerat adipisci voluptate quidem. Ullam.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+                reprehenderit inventore vel alias nemo dolores quasi quia rerum
+                rem accusamus. Voluptatem nam autem porro repellendus animi
+                ratione rem eveniet? Quibusdam.
               </p>
             </div>
           </div>
