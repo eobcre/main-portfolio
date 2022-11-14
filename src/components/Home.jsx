6888c49921daf003ad53818fec5f2112e151bdf7
eobcre <eobcre@gmail.com>
2022-11-14 10:34:33 -0500

    Fade-up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,85 +1,87 @@
-import React from "react";
-import Header from "./Header";
+import React from 'react';
+// Components
+import Header from './Header';
 // Images
-import Brackets from "../img/assets/brackets.png";
-import Bracket from "../img/assets/bracket.png";
-import Bracket_2 from "../img/assets/bracket_2.png";
+import Brackets from '../assets/brackets.png';
+import Bracket from '../assets/bracket.png';
+import Bracket_2 from '../assets/bracket_2.png';
 
 const Home = () => {
+  // JSX
   return (
-    <div className="h-screen w-screen bg-webTop bg-center bg-zoom table-cell align-middle">
+    <div className='w-screen h-screen bg-webTop bg-center bg-zoom table-cell align-middle'>
       <Header />
       {/* Main contents */}
-      <div className="text-center py-8">
-        <p className="text-7xl text-dark-gray tracking-wide py-4">
+      <div className='text-center py-8'>
+        <p className='fadeup box text-7xl text-dark-gray font-bebas py-4'>
           Emmy Manning
         </p>
-        <p className="text-2xl text-dark-gray">
-          Love to build creative and magical things.
+        <p className='fadeup box delay_1 text-xl text-dark-gray font-bebas'>
+          Love to build creative and magical things
         </p>
       </div>
 
       {/* Card */}
-      <div className="glass mx-auto w-380 md:w-600">
-        <span className="md:absolute bottom-3/4 -left-12 rotate-12 w-24 h-8 bg-clear-tape md:border-l-2 md:border-r-2 border-dotted"></span>
-        <span className="md:absolute bottom-2/4 -right-12 -rotate-12 w-24 h-8 bg-clear-tape md:border-l-2 md:border-r-2 border-dotted"></span>
-        <main className="max-w-21 mx-auto">
+      <div className='fadeup box delay_2 glass mx-auto w-380 md:w-600'>
+        <span className='md:absolute bottom-3/4 -left-12 rotate-12 w-24 h-8 bg-clear-tape md:border-l-2 md:border-r-2 border-dotted'></span>
+        <span className='md:absolute bottom-2/4 -right-12 -rotate-12 w-24 h-8 bg-clear-tape md:border-l-2 md:border-r-2 border-dotted'></span>
+        <main className='max-w-21 mx-auto'>
           {/* GitHub */}
-          <div className="grid grid-cols-10 mt-6">
-            <span className="effect-st col-start-2 col-span-2">
+          <div className='grid grid-cols-10 mt-6'>
+            <span className='effect-st col-start-2 col-span-2'>
               <a
-                href="https://github.com/eobcre"
-                target="_blank"
-                rel="noopener noreferrer"
+                href='https://github.com/eobcre'
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 GitHub
               </a>
             </span>
             {/* Portfolio */}
-            <span className="effect-st col-start-7 col-span-3">
+            <span className='effect-st col-start-7 col-span-3'>
               <a
-                href="https://google.com"
-                target="_blank"
-                rel="noopener noreferrer"
+                href='https://google.com'
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 Portfolio
               </a>
             </span>
           </div>
 
-          <img src={Brackets} alt="Icon" />
+          <img src={Brackets} alt='Icon' />
 
           {/* eobcre@emanning.dev */}
           <div>
-            <p className="text-center text-5xl text-light-gray">
+            <p className='text-center text-5xl text-dark-gray font-bebas opacity-50'>
               eobcre@emanning.dev
             </p>
           </div>
 
-          <img src={Bracket_2} alt="Icon" />
+          <img src={Bracket_2} alt='Icon' />
 
           {/* Instagram */}
-          <div className="grid grid-cols-9">
-            <span className="effect-st col-end-8 col-span-2">
+          <div className='grid grid-cols-9'>
+            <span className='effect-st col-end-8 col-span-2'>
               <a
-                href="https://www.instagram.com/emanning.dev/"
-                target="_blank"
-                rel="noopener noreferrer"
+                href='https://www.instagram.com/emanning.dev/'
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 Instagram
               </a>
             </span>
           </div>
 
-          <img src={Bracket} alt="Icon" />
+          <img src={Bracket} alt='Icon' />
 
           {/* Email */}
-          <div className="sub-email grid justify-center mb-4">
-            <span className="effect-st">
+          <div className='sub-email grid justify-center mb-4'>
+            <span className='effect-st'>
               <a
-                href="mailto:eobcre@gmail.com"
-                target="_blank"
-                rel="noopener noreferrer"
+                href='mailto:eobcre@gmail.com'
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 Email
               </a>
