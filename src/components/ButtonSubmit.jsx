66693cf61efeeb50fb4d79a84ecd8020eef72884
eobcre <eobcre@gmail.com>
2022-11-23 23:08:58 -0500

    New component added

---
+++ src/components/ButtonSubmit.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+const ButtonSubmit = (props) => {
+  return (
+    <div className='flex justify-center py-8'>
+      <button
+        className='button px-12 py-2 bg-dark-gray text-white font-bebas hover:bg-transparent hover:text-dark-gray'
+        type='submit'
+      >
+        <span className='text'>{props.name}</span>
+        <div className='pen_1'></div>
+        <div className='pen_2'></div>
+      </button>
+    </div>
+  );
+};
+
+export default ButtonSubmit;
