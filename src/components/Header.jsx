24f9ea3b81a771a5ca862796c78f235f784099cb
eobcre <eobcre@gmail.com>
2022-11-26 22:29:33 -0500

    Reviewed and fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,93 +12,92 @@ export default function Header() {
   const toggle = () => setNavbar(!navbar);
   // JSX
   return (
-    <>
-      <nav className='fixed top-0 z-10'>
-        <div className='glass w-screen justify-between px-4 mx-auto md:items-center md:flex md:px-8'>
-          <div>
-            <div className='flex items-center justify-between py-2 md:block'>
-              <a href='./'>
-                <img src={Logo} alt='Logo' width='40' height='40' />
-              </a>
-              <div className='md:hidden'>
-                <button
-                  className='p-2 text-white rounded-md outline-none'
-                  onClick={toggle}
-                >
-                  {navbar ? (
-                    <HiX className='text-4xl' /> // Close icon
-                  ) : (
-                    <HiMenuAlt4 className='text-4xl' /> // Open icon
-                  )}
-                </button>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div
-              className={`flex-1 justify-self-center pb-8 mt-8 md:block md:pb-0 md:mt-0 ${
-                navbar ? 'block' : 'hidden'
-              }`}
+    <nav className='fixed top-0 z-10'>
+      <div className='glass w-screen px-4 md:flex md:justify-between md:items-center'>
+        {/* Logo */}
+        <div className='flex justify-between items-center py-2'>
+          <a href='./'>
+            <img src={Logo} alt='Logo' width='40' height='40' />
+          </a>
+          {/* Mobile menu button */}
+          <div className='md:hidden'>
+            <button
+              className='text-white rounded-md outline-none p-2'
+              onClick={toggle}
             >
-              <ul className='text-dark-gray font-bebas items-center justify-center space-y-8 md:text-white md:flex md:space-x-6 md:space-y-0'>
-                {/* About */}
-                <li className='text-center text-xl hover:opacity-40 2xl:text-2xl'>
-                  <Link
-                    activeClass='active'
-                    to='About'
-                    spy={true}
-                    smooth={true}
-                    offset={-50}
-                    duration={500}
-                    onClick={toggle}
-                  >
-                    About
-                  </Link>
-                </li>
-                {/* My Work */}
-                <li className='text-center text-xl hover:opacity-40 2xl:text-2xl'>
-                  <Link
-                    activeClass='active'
-                    to='MyWork'
-                    spy={true}
-                    smooth={true}
-                    offset={-50}
-                    duration={500}
-                    onClick={toggle}
-                  >
-                    My Work
-                  </Link>
-                </li>
-                {/* Contact */}
-                <li className='text-center text-xl hover:opacity-40 2xl:text-2xl'>
-                  <Link
-                    activeClass='active'
-                    to='Contact'
-                    spy={true}
-                    smooth={true}
-                    offset={-50}
-                    duration={500}
-                    onClick={toggle}
-                  >
-                    Contact
-                  </Link>
-                </li>
-                <li className='text-center text-xl hover:opacity-40 2xl:text-2xl'>
-                  <a href={PDF} target='_blank' rel='noopener noreferrer'>
-                    Resume
-                  </a>
-                </li>
-              </ul>
-            </div>
+              {navbar ? (
+                <HiX className='text-4xl' /> // Close icon
+              ) : (
+                <HiMenuAlt4 className='text-4xl' /> // Open icon
+              )}
+            </button>
           </div>
         </div>
-        {/* Copyright */}
-        <div className='fixed bottom-0 text-dark-gray text-2xl [writing-mode:vertical-rl] [text-orientation:sideways-right] px-4 py-8'>
-          <p className='hidden md:block md:font-bebas'>
-            &copy; 2023 Emmy Manning
-          </p>
+
+        {/* Nav list */}
+        <div className={`pb-8 md:block md:pb-0 ${navbar ? 'block' : 'hidden'}`}>
+          <ul className='text-dark-gray font-bebas justify-center items-center space-y-8 md:text-white md:flex md:space-x-6 md:space-y-0'>
+            {/* About */}
+            <li className='nav-items-st'>
+              <Link
+                activeClass='active'
+                to='About'
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={500}
+                onClick={toggle}
+              >
+                About
+              </Link>
+            </li>
+
+            {/* My Work */}
+            <li className='nav-items-st'>
+              <Link
+                activeClass='active'
+                to='MyWork'
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={500}
+                onClick={toggle}
+              >
+                My Work
+              </Link>
+            </li>
+
+            {/* Contact */}
+            <li className='nav-items-st'>
+              <Link
+                activeClass='active'
+                to='Contact'
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={500}
+                onClick={toggle}
+              >
+                Contact
+              </Link>
+            </li>
+
+            {/* Resume */}
+            <li className='nav-items-st'>
+              <a href={PDF} target='_blank' rel='noopener noreferrer'>
+                Resume
+              </a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </>
+      </div>
+
+      {/* Copyright */}
+      <div className='fixed bottom-0 text-dark-gray text-2xl [writing-mode:vertical-rl] [text-orientation:sideways-right] px-4 py-8'>
+        <p className='hidden md:block md:font-bebas'>
+          &copy; 2023 Emmy Manning
+        </p>
+      </div>
+    </nav>
   );
 }
