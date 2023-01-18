4506407dca3aba586371d8b9677cb7375399a968
eobcre <eobcre@gmail.com>
2023-01-18 10:01:20 -0500

    salon img added

--- src/sections/MyWork.jsx
+++ src/sections/MyWork.jsx
@@ -7,6 +7,7 @@ import TodoLogo from '../assets/to-do.png';
 import SalonLogo from '../assets/salon.jpg';
 import WeatherLogo from '../assets/weather.jpg';
 import TodoApp from '../assets/to-do-app.png';
+import Salon from '../assets/salon.png';
 import WeatherApp from '../assets/weather-app.png';
 
 export const MyWork = (prop) => {
@@ -43,7 +44,7 @@ export const MyWork = (prop) => {
         <div className='md:flex gap-10 mt-28'>
           <div className='w-auto backdrop-blur md:w-2/4'>
             <span className='hidden md:block absolute md:-top-4 left-inset40 rotate-45 w-24 h-8 bg-clear-tape border-l-2 border-r-2 border-dotted'></span>
-            <img className='hidden md:block' src={WeatherApp} alt='WebImage' />
+            <img className='hidden md:block' src={Salon} alt='WebImage' />
           </div>
           <div className='md:w-2/4'>
             <img
