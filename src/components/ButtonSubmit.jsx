9fc72d84f8d6cee558850ca173034064874f7465
eobcre <eobcre@gmail.com>
2023-02-04 11:11:37 -0500

    removed button html and style, added new button style

--- src/components/ButtonSubmit.jsx
+++ src/components/ButtonSubmit.jsx
@@ -1,15 +1,11 @@
-import React from 'react';
-
 export const ButtonSubmit = (props) => {
   return (
     <div className='flex justify-center py-8'>
       <button
-        className='button px-12 py-2 bg-dark-gray text-white font-bebas hover:bg-transparent hover:text-dark-gray'
+        className='bg-dark-gray font-bebas text-white text-2xl hover:scale-90 translate-x-4 px-12 py-2'
         type='submit'
       >
         <span className='text'>{props.name}</span>
-        <div className='pen_1'></div>
-        <div className='pen_2'></div>
       </button>
     </div>
   );
