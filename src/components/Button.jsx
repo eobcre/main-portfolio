10d474985d3e5f49421fafa93c538dcebc9b6ac3
eobcre <eobcre@gmail.com>
2023-02-04 11:07:56 -0500

    prop spelling fixed, removed button html and style, added new button style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,13 @@
-import React from 'react';
-
-export const Button = (prop) => {
+export const Button = (props) => {
   return (
     <div className='flex items-center pt-8'>
       <a
-        href={prop.link}
+        href={props.link}
         target='_blank'
         rel='noopener noreferrer'
-        className='button px-20 py-2 bg-dark-gray text-white hover:bg-transparent hover:text-dark-gray'
+        className=' bg-dark-gray text-white text-2xl hover:scale-90 translate-x-4 px-20 py-2'
       >
-        <span className='text font-bebas'>{prop.name}</span>
-        <div className='pen_1'></div>
-        <div className='pen_2'></div>
+        <span className='text font-bebas'>{props.name}</span>
       </a>
     </div>
   );
