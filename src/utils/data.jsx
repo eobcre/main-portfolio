fc85f291851638616f5d47cfff559f5928ac8d20
eobcre <eobcre@gmail.com>
2023-03-09 14:25:01 -0500

    data my work

---
+++ src/utils/data.jsx
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    name: 'Salon Website',
+    src: '../../work_assets/salon.png',
+    logo: '../../work_assets/salon.jpg',
+    description:
+      'The website was made for my friend who is a hair stylist in San Diego, CA. It is actually the first website that I created and re-created in Vite React, styled in Tailwind. She has exellent skills and technique. Check it out!',
+  },
+  {
+    name: 'Omikuji',
+    src: '../../work_assets/omikuji-app.png',
+    logo: '../../work_assets/omikuji.png',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde quidem repudiandae natus temporibus dolorum vero incidunt optio vel ullam. Eaque tempora maiores inventore ullam. Repellat dignissimos ipsum iste sunt suscipit.',
+  },
+  {
+    name: 'To-Do-App',
+    src: '../../work_assets/to-do-app.png',
+    logo: '../../work_assets/to-do.png',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde quidem repudiandae natus temporibus dolorum vero incidunt optio vel ullam. Eaque tempora maiores inventore ullam. Repellat dignissimos ipsum iste sunt suscipit.',
+  },
+];
