b10bd95c48a59a6267f17b080db0de6d8dec3adb
eobcre <eobcre@gmail.com>
2023-03-09 18:04:25 -0500

    button styles fixed

--- src/components/ButtonSubmit.jsx
+++ src/components/ButtonSubmit.jsx
@@ -2,7 +2,7 @@ export const ButtonSubmit = (props) => {
   return (
     <div className='flex justify-center py-8'>
       <button
-        className='bg-dark-gray font-bebas text-white text-2xl hover:text-light-blue px-20 py-2'
+        className='bg-dark-gray font-bebas text-white text-2xl hover:text-light-blue transition-all duration-300 px-20 py-2'
         type='submit'
       >
         <span className='text'>{props.name}</span>
