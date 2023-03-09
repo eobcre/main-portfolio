637243e2ece7d7797de6463c9e595454f5643f11
eobcre <eobcre@gmail.com>
2023-03-09 17:41:14 -0500

    tech1, 2, 3, 4, 5 added

--- src/sections/MyWork.jsx
+++ src/sections/MyWork.jsx
@@ -8,30 +8,42 @@ export const MyWork = () => {
   return (
     <div className='h-auto bg-light-blue py-20'>
       <div className='mx-auto w-4/5'>
-        <h1 className='sub-st md:mb-16'>My Work</h1>
+        <h1 className='sub-st'>My Work</h1>
         {/* work list */}
-        {data.map(({ name, src, logo, description }, index) => (
-          <div key={index} className='md:flex gap-10 mt-28'>
-            <div className='w-auto backdrop-blur md:w-2/4'>
-              <span className='hidden md:block absolute md:-top-4 left-inset40 rotate-45 w-24 h-8 bg-clear-tape border-l-2 border-r-2 border-dotted'></span>
-              <img src={src} alt={`${index}`} className='hidden md:block' />
+        {data.map(
+          (
+            { name, src, logo, description, tech1, tech2, tech3, tech4, tech5 },
+            index
+          ) => (
+            <div key={index} className='md:flex gap-10 md:mt-24'>
+              <div className='w-auto backdrop-blur md:w-2/4'>
+                <span className='hidden md:block absolute md:-top-4 left-inset40 rotate-45 w-24 h-8 bg-clear-tape border-l-2 border-r-2 border-dotted'></span>
+                <img src={src} alt={`${index}`} className='hidden md:block' />
+              </div>
+              <div className='md:w-2/4'>
+                <img
+                  src={logo}
+                  alt={`${index}`}
+                  className='mt-10 md:hidden'
+                  width='100'
+                  height='100'
+                />
+                <h2 className='my-work-title-st'>{name}</h2>
+                <FadeIn>
+                  <p className='my-work-st'>{description}</p>
+                  <div className='flex gap-4'>
+                    <p className='text-white text-sm'>{tech1}</p>
+                    <p className='text-white text-sm'>{tech2}</p>
+                    <p className='text-white text-sm'>{tech3}</p>
+                    <p className='text-white text-sm'>{tech4}</p>
+                    <p className='text-white text-sm'>{tech5}</p>
+                  </div>
+                  <Button name='See The App' link='#' />
+                </FadeIn>
+              </div>
             </div>
-            <div className='md:w-2/4'>
-              <img
-                src={logo}
-                alt={`${index}`}
-                className='md:hidden'
-                width='100'
-                height='100'
-              />
-              <h2 className='my-work-title-st'>{name}</h2>
-              <FadeIn>
-                <p className='my-work-st'>{description}</p>
-              </FadeIn>
-              <Button name='See The App' link='#' />
-            </div>
-          </div>
-        ))}
+          )
+        )}
       </div>
     </div>
   );
