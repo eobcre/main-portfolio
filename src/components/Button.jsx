9f78253fd821e2bd073e0328190ebca45e3edc3b
eobcre <eobcre@gmail.com>
2023-03-09 17:36:50 -0500

    button styles fixed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 export const Button = (props) => {
   return (
-    <div className='flex items-center pt-8'>
+    <div className='pt-8'>
       <a
         href={props.link}
         target='_blank'
         rel='noopener noreferrer'
-        className=' bg-dark-gray text-white text-2xl hover:scale-90 translate-x-4 delay-150 px-20 py-2'
+        className='bg-dark-gray text-white text-2xl hover:text-light-blue px-20 py-2'
       >
         <span className='text font-bebas'>{props.name}</span>
       </a>
