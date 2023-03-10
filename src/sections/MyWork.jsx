bb40ebbc10a23f39645851dfa914085197855883
eobcre <eobcre@gmail.com>
2023-03-09 19:26:15 -0500

    styles fixed

--- src/sections/MyWork.jsx
+++ src/sections/MyWork.jsx
@@ -15,7 +15,7 @@ export const MyWork = () => {
             { name, src, logo, description, tech1, tech2, tech3, tech4, tech5 },
             index
           ) => (
-            <div key={index} className='md:flex gap-10 md:mt-24'>
+            <div key={index} className='md:flex gap-10 mt-20 md:mt-32'>
               <div className='w-auto backdrop-blur md:w-2/4'>
                 <span className='hidden md:block absolute md:-top-4 left-inset40 rotate-45 w-24 h-8 bg-clear-tape border-l-2 border-r-2 border-dotted'></span>
                 <img src={src} alt={`${index}`} className='hidden md:block' />
@@ -24,7 +24,7 @@ export const MyWork = () => {
                 <img
                   src={logo}
                   alt={`${index}`}
-                  className='mt-10 md:hidden'
+                  className='md:hidden'
                   width='100'
                   height='100'
                 />
