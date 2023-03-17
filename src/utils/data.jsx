65c4b14cc93675f6f46316bbbfc5e3ddba8c85be
eobcre <eobcre@gmail.com>
2023-03-17 14:59:23 -0400

    descriptions fixed

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -4,7 +4,7 @@ export const data = [
     src: '../../work_assets/salon.png',
     logo: '../../work_assets/salon.jpg',
     description:
-      'The website was made for my friend who is a hair stylist in San Diego, CA. She has exellent skills and technique. Check it out!',
+      'The website was made for my friend, who is a hair stylist in San Diego, CA. She has exellent skills and techniques. Please check it out!',
     tech1: 'React',
     tech2: 'Vite',
     tech3: 'Java Script',
@@ -16,7 +16,7 @@ export const data = [
     src: '../../work_assets/omikuji-app.png',
     logo: '../../work_assets/omikuji.png',
     description:
-      'Omikuji is a paper slip with the result of fortune-telling. It supposed to predict your future and they are in shrines / temples in Japan. Now you can try it on an app. Find out your fortune!',
+      'Omikuji is a paper slip that contains the result of fortune-telling. It is believed to predict your future and is commonly found in shrines and temples throughout Japan. Now, you can try it on an app and find out your fortune! ',
     tech1: 'React',
     tech2: 'Vite',
     tech3: 'Java Script',
@@ -28,7 +28,7 @@ export const data = [
     src: '../../work_assets/to-do-app.png',
     logo: '../../work_assets/to-do.png',
     description:
-      'A web app to organize your to do, a list of tasks you need to complete. It is helpful in planning our daily tasks.',
+      'This is a web app to organize your to-do list - a list of tasks you need to complete. It is helpful in planning your daily tasks.',
     tech1: 'JavaScript',
     tech2: 'HTML',
     tech3: 'CSS',
