79dfdab0bd19694bec73cba0c0868b5c4c8b3270
eobcre <eobcre@gmail.com>
2023-03-19 18:45:23 -0400

    file named changed

---
+++ src/components/ScrollFadeIn.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+export const ScrollFadeIn = (props) => {
+  const [visible, setVisible] = useState(false);
+  const documentRef = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        return entry.isIntersecting && setVisible(true);
+      });
+    });
+    observer.observe(documentRef.current);
+  }, []);
+
+  //  JSX
+  return (
+    <div
+      ref={documentRef}
+      className={`ScrollFadeIn ${visible ? 'is-visible' : ''}`}
+    >
+      {props.children}
+    </div>
+  );
+};
