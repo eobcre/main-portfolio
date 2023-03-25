7e6ff78c2f569fde6825ee05b693532c135e961e
eobcre <eobcre@gmail.com>
2023-03-24 22:12:27 -0400

    footer color fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -90,7 +90,7 @@ export const Header = () => {
       </div>
       {/* Copyright */}
       <div className='fixed bottom-0 text-dark-gray text-2xl [writing-mode:vertical-rl] [text-orientation:sideways-right] px-4 py-8'>
-        <p className='hidden md:block md:font-bebas'>
+        <p className='hidden md:block md:text-dark-gray font-bebas'>
           &copy; 2023 Emmy Manning
         </p>
       </div>
