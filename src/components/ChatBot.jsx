610d312d47843aceb0e61851c3f9a2f1150ad2aa
eobcre <eobcre@gmail.com>
2023-03-29 16:22:18 -0400

    dialogflow chat widget

---
+++ src/components/ChatBot.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+
+export const ChatBot = () => {
+  // prevent duplicate elements dl-messenger-list
+  useEffect(() => {
+    if (!window.dfMessengerLoaded) {
+      const script = document.createElement('script');
+      script.src =
+        'https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1';
+      script.async = true;
+      document.body.appendChild(script);
+      window.dfMessengerLoaded = true;
+    }
+    // customize chat widget height
+    const dfMessenger = document.querySelector('df-messenger');
+    dfMessenger.addEventListener('df-messenger-loaded', function () {
+      const dfMessengerChat =
+        dfMessenger.shadowRoot.querySelector('df-messenger-chat');
+      const style = document.createElement('style');
+      style.innerHTML =
+        'div.chat-wrapper[opened="true"]{ height:470px; max-height:470px; }';
+      dfMessengerChat.shadowRoot.appendChild(style);
+    });
+  }, []);
+
+  return (
+    <>
+      <df-messenger
+        intent='WELCOME'
+        chat-title='Emmy'
+        chat-icon='https://img.icons8.com/material-outlined/36/ffffff/filled-chat.png'
+        agent-id={process.env.REACT_APP__DIALOG_ID}
+        language-code='en'
+      ></df-messenger>
+    </>
+  );
+};
