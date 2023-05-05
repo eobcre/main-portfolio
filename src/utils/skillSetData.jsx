ce340fb6667d24097fee71541d2aca1e1b76bfa3
eobcre <eobcre@gmail.com>
2023-05-05 13:19:53 -0400

    created skill set data for about page

---
+++ src/utils/skillSetData.jsx
@@ -0,0 +1,35 @@
+export const skillSetData = [
+  {
+    skill: 'HTML',
+  },
+  {
+    skill: 'CSS',
+  },
+  {
+    skill: 'Sass',
+  },
+  {
+    skill: 'Tailwind',
+  },
+  {
+    skill: 'JavaScript',
+  },
+  {
+    skill: 'TypeScript',
+  },
+  {
+    skill: 'Node.js',
+  },
+  {
+    skill: 'React.js',
+  },
+  {
+    skill: 'Git',
+  },
+  {
+    skill: 'GitHub',
+  },
+  {
+    skill: 'Visual Studio',
+  },
+];
