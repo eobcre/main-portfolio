54c6e6a31826d0a0679f2595a09831964560027b
eobcre <eobcre@gmail.com>
2023-06-12 17:16:03 -0400

    changed button name

--- src/sections/MyWork.jsx
+++ src/sections/MyWork.jsx
@@ -37,7 +37,7 @@ const MyWork = () => {
                   <span className='tech-st'>{data.tech4}</span>
                   <span className='tech-st'>{data.tech5}</span>
                 </div>
-                <Button name='See The App' link={data.url} />
+                <Button name='View' link={data.url} />
               </ScrollFadeIn>
             </div>
           </div>
