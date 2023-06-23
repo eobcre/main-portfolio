54fc5ce03be0c49f706ade4485384249b60e62ad
eobcre <eobcre@gmail.com>
2023-06-23 18:55:58 -0400

    changed sub title

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
           Emmy Manning
         </h1>
         <h2 className='mainFadeup mainFadeupDelay_1 text-xl text-dark-gray font-bebas'>
-          Love to build creative and magical things
+          Frontend Developer | Pittsburgh, PA
         </h2>
       </div>
 
